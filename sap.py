70a88be01dbe99c8dc40d04b15a5e8da56b1afbd
bryan.pinto <bryan.pinto@conecthus.org.br>
2023-08-13 11:47:37 -0400

    feat: sap array

--- sap.py
+++ sap.py
@@ -4,21 +4,39 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def get_produtividade():
-    while True:
-        produtividade = {
-            "maquina": {
-                "nome": "Máquina de produção 1",
-                "id": 1234,
-                "produtividade": {
-                    "data": "2023-07-12",
-                    "quantidade_esperada": random.uniform(0.01, 0.10),
-                }
-            }
+production_order = [
+    {
+        "quantidade_planejada": "500900",
+        "op_number": 123456789,
+        "produtividade": {
+            "data": "2023-07-12",
+            "quantidade_esperada": 9000,
         }
-        return jsonify(produtividade)
-        time.sleep(1)
+    },
+    {
+        "quantidade_planejada": "500900",
+        "op_number": 987654321,
+        "produtividade": {
+            "data": "2023-07-12",
+            "quantidade_esperada": 8000,
+        }
+    },
+    {
+        "quantidade_planejada": "500900",
+        "op_number": 192837465,
+        "produtividade": {
+            "data": "2023-07-12",
+            "quantidade_esperada": 6000,
+        }
+    }
+]
+
+@app.route('/<int:op_number>')
+def get_produtividade(op_number):
+    for op in production_order:
+        if op['op_number'] == op_number:
+            return jsonify(op)
+    return jsonify({"error": "Op não encontrada"}), 404
 
 if __name__ == '__main__':
     app.run()
