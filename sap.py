9d76ac4662ab5beb836efda984f4063ad5e27a9f
bryan neves pinto <bryan.pinto@conecthus.org.br>
2023-08-24 14:54:47 -0400

    fix: created objects

--- sap.py
+++ sap.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-const production_order = [
+production_order = [
     {
         "op_number": "123456789",
         "production_goal_expected": "500",
